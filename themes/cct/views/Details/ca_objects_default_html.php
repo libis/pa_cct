69e843b359904096bab8a1e061891496a8dc0922
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2016-11-21 16:16:03 +0100

    object detail page

--- themes/cct/views/Details/ca_objects_default_html.php
+++ themes/cct/views/Details/ca_objects_default_html.php
@@ -44,30 +44,7 @@
 	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
 		<div class="container"><div class="row">
 			<div class='col-sm-6 col-md-6 col-lg-5 col-lg-offset-1'>
-				{{{representationViewer}}}
-				
-				
 				<div id="detailAnnotations"></div>
-				
-				<?php print caObjectRepresentationThumbnails($this->request, $this->getVar("representation_id"), $t_object, array("returnAs" => "bsCols", "linkTo" => "carousel", "bsColClasses" => "smallpadding col-sm-3 col-md-3 col-xs-4")); ?>
-				
-<?php
-				# Comment and Share Tools
-				if ($vn_comments_enabled | $vn_share_enabled) {
-						
-					print '<div id="detailTools">';
-					if ($vn_comments_enabled) {
-?>				
-						<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
-						<div id='detailComments'><?php print $this->getVar("itemComments");?></div><!-- end itemComments -->
-<?php				
-					}
-					if ($vn_share_enabled) {
-						print '<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>'.$this->getVar("shareLink").'</div><!-- end detailTool -->';
-					}
-					print '</div><!-- end detailTools -->';
-				}				
-?>
 			</div><!-- end col -->
 			
 			<div class='col-sm-6 col-md-6 col-lg-5'>
@@ -76,36 +53,47 @@
 				<HR>
 				
 				{{{<ifdef code="ca_objects.measurementSet.measurements">^ca_objects.measurementSet.measurements (^ca_objects.measurementSet.measurementsType)</ifdef><ifdef code="ca_objects.measurementSet.measurements,ca_objects.measurementSet.measurements"> x </ifdef><ifdef code="ca_objects.measurementSet.measurements2">^ca_objects.measurementSet.measurements2 (^ca_objects.measurementSet.measurementsType2)</ifdef>}}}
-				
-				
+
+
+				{{{<ifdef code="ca_objects.preferred_labels"><H6>Title:</H6>^ca_objects.preferred_labels<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.marc242a"><H6>Title:</H6>^ca_objects.marc242a<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.non_preferred_labels">^ca_objects.non_preferred_labels<br/></ifdef>}}}
 				{{{<ifdef code="ca_objects.idno"><H6>Identifer:</H6>^ca_objects.idno<br/></ifdef>}}}
-				{{{<ifdef code="ca_objects.containerID"><H6>Box/series:</H6>^ca_objects.containerID<br/></ifdef>}}}				
-				
-				{{{<ifdef code="ca_objects.description">
-					<span class="trimText">^ca_objects.description</span>
-				</ifdef>}}}
-				
-				
-				{{{<ifdef code="ca_objects.dateSet.setDisplayValue"><H6>Date:</H6>^ca_objects.dateSet.setDisplayValue<br/></ifdev>}}}
+
+				{{{<ifcount code="ca_entities" min="1" max="1"><H6>Author</H6></ifcount>}}}
+				{{{<ifcount code="ca_entities" min="2"><H6>Authors</H6></ifcount>}}}
+				{{{<unit relativeTo="ca_entities" restrictToRelationshipTypes="Author" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
+
+				{{{<ifdef code="ca_objects.marc520a_cont"><H6>Contents:</H6>^ca_objects.marc520a_cont<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.marc529a"><H6>Notes on Contents:</H6>^ca_objects.marc529a<br/></ifdef>}}}
+
+				{{{<ifcount code="ca_list_items" min="1"><H6>Category</H6></ifcount>}}}
+				{{{<unit relativeTo="ca_list_items" restrictToRelationshipTypes="category" delimiter="<br/>">^ca_list_items.preferred_labels.name_plural</unit>}}}
+
+				{{{<ifdef code="ca_objects.marc529a"><H6>Document Type:</H6>^ca_objects.marc655a<br/></ifdef>}}}
+				{{{<ifdef code="ca_objects.marc529a"><H6>Type of Source:</H6>^ca_objects.marc900a<br/></ifdef>}}}
+				 	
 				
 				<hr></hr>
 					<div class="row">
-						<div class="col-sm-6">		
-							{{{<ifcount code="ca_entities" min="1" max="1"><H6>Related person</H6></ifcount>}}}
-							{{{<ifcount code="ca_entities" min="2"><H6>Related people</H6></ifcount>}}}
-							{{{<unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit>}}}
-							
-							
-							{{{<ifcount code="ca_places" min="1" max="1"><H6>Related place</H6></ifcount>}}}
-							{{{<ifcount code="ca_places" min="2"><H6>Related places</H6></ifcount>}}}
-							{{{<unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit>}}}
-							
-							{{{<ifcount code="ca_list_items" min="1" max="1"><H6>Related Term</H6></ifcount>}}}
-							{{{<ifcount code="ca_list_items" min="2"><H6>Related Terms</H6></ifcount>}}}
-							{{{<unit relativeTo="ca_list_items" delimiter="<br/>">^ca_list_items.preferred_labels.name_plural</unit>}}}
-							
-							{{{<ifcount code="ca_objects.LcshNames" min="1"><H6>LC Terms</H6></ifcount>}}}
-							{{{<unit delimiter="<br/>"><l>^ca_objects.LcshNames</l></unit>}}}
+						<div class="col-sm-6">
+							<?php
+							# Comment and Share Tools
+							if ($vn_comments_enabled | $vn_share_enabled) {
+
+								print '<div id="detailTools">';
+								if ($vn_comments_enabled) {
+									?>
+									<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
+									<div id='detailComments'><?php print $this->getVar("itemComments");?></div><!-- end itemComments -->
+									<?php
+								}
+								if ($vn_share_enabled) {
+									print '<div class="detailTool"><span class="glyphicon glyphicon-share-alt"></span>'.$this->getVar("shareLink").'</div><!-- end detailTool -->';
+								}
+								print '</div><!-- end detailTools -->';
+							}
+							?>
 						</div><!-- end col -->				
 						<div class="col-sm-6 colBorderLeft">
 							{{{map}}}
@@ -119,6 +107,7 @@
 			{{{nextLink}}}
 		</div><!-- end detailNavBgLeft -->
 	</div><!-- end col -->
+
 </div><!-- end row -->
 
 <script type='text/javascript'>
