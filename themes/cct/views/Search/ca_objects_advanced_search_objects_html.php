5d16f23e76ed4304f3d53de4bebfdfe48cd03681
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-02-13 10:11:54 +0100

    advanced search form fields width

--- themes/cct/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/cct/views/Search/ca_objects_advanced_search_objects_html.php
@@ -12,137 +12,137 @@
     <div style="float: right; margin-left: 20px; margin-right: 20px;">{{{reset%label=Reset}}}</div>
     <div style="float: right;">{{{submit%label=Search}}}</div>
     <br style="clear: both;"/>
-	
+
 	<div class='advancedContainer'>
 		<div class="advancedSearchField">
 			Free Text:<br/>
-			{{{_fulltext%width=210px&height=50px&placeholder='Type word or phrase and click Search'}}}
+			{{{_fulltext%width=100%&height=50px&placeholder='Type word or phrase and click Search'}}}
 		</div>
         <div class="advancedSearchField">
             Identifier:<br/>
-            {{{ca_objects.idno%width=210px}}}
+            {{{ca_objects.idno%width=100%}}}
         </div>
         <div class="advancedSearchField">
             Title:<br/>
-            {{{ca_objects.preferred_labels.name%width=210px}}}
+            {{{ca_objects.preferred_labels.name%width=100%}}}
         </div>
         <div class="advancedSearchField">
             Title (Series):<br/>
-            {{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=partOfSeries&width=210px}}}
+            {{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=partOfSeries&width=100%}}}
         </div>
 		<div class="advancedSearchField">
 			Authors (all):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut,clb,edt,edc,imp,trl,oth,ppf,com,ctb&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut,clb,edt,edc,imp,trl,oth,ppf,com,ctb&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Author(aut):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Translator(trl):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=trl&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=trl&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Collaborator(clb):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=clb&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=clb&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Nihil Obstat(nio):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=nio&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=nio&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			*Imprimatur(imp):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=imp&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=imp&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Pre/Post Faces(ppf):<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=ppf&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=ppf&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Periodical:<br/>
-			{{{ca_objects.marc773.marc773a%width=210px&height=25px}}}
+			{{{ca_objects.marc773.marc773a%width=100%&height=25px}}}
 		</div>
 		<div class="advancedSearchField">
 			Impressum (date):<br/>
-			{{{ca_objects.marc260c%width=210px&height=25px}}}
+			{{{ca_objects.marc260c%width=100%&height=25px}}}
 		</div>
 		<div class="advancedSearchField">
 			Impressum (place):<br/>
-			{{{ca_places.preferred_labels%restrictToRelationshipTypes=printedIn&width=210px}}}
+			{{{ca_places.preferred_labels%restrictToRelationshipTypes=printedIn&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Imprimatur:<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=imp&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=imp&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Description based on Title:<br/>
-			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=descriptionTitle&width=210px}}}
+			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=descriptionTitle&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Contents:<br/>
-			{{{ca_objects.marc520a_cont%width=210px&height=25px}}}
+			{{{ca_objects.marc520a_cont%width=100%&height=25px}}}
 		</div>
 		<div class="advancedSearchField">
 			Adaptations from West:<br/>
-			{{{ca_objects.marc795.marc795a%width=210px&height=25px}}}
+			{{{ca_objects.marc795.marc795a%width=100%&height=25px}}}
 		</div>
 		<div class="advancedSearchField">
 			Facsimile editions:<br/>
-			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=facsimile&width=210px}}}
+			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=facsimile&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Reprints(place):<br/>
-			{{{ca_objects_x_places.preferred_labels%restrictToRelationshipTypes=printedIn&width=210px}}}
+			{{{ca_objects_x_places.preferred_labels%restrictToRelationshipTypes=printedIn&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Reprints in Series:<br/>
-			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=reprintSeries&width=210px}}}
+			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=reprintSeries&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Library copies:<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=libraryCopy&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=libraryCopy&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Ancient List:<br/>
-			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=ancientList&width=210px}}}
+			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=ancientList&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Modern List:<br/>
-			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=modernList&width=210px}}}
+			{{{ca_objects_x_objects.preferred_labels%restrictToRelationshipTypes=modernList&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Reviews:<br/>
-			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=reviewer&width=210px}}}
+			{{{ca_entities.preferred_labels%restrictToRelationshipTypes=reviewer&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Category:<br/>
-			{{{ca_list_items.preferred_labels.name_plural%restrictToRelationshipTypes=category&width=210px}}}
+			{{{ca_list_items.preferred_labels.name_plural%restrictToRelationshipTypes=category&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Source:<br/>
-			{{{ca_objects.marc900a%width=210px}}}
+			{{{ca_objects.marc900a%width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Type:<br/>
-			{{{ca_objects.type_id%width=210px}}}
+			{{{ca_objects.type_id%width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Document Type:<br/>
-			{{{ca_objects.marc655a%width=210px}}}
+			{{{ca_objects.marc655a%width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Translation:<br/>
-			{{{ca_objects_x_objects.link_translation_lkrr%excludeRelationshipTypes=r77301,r77302,r77300,r77301,r77500,r77501&width=210px}}}
+			{{{ca_objects_x_objects.link_translation_lkrr%excludeRelationshipTypes=r77301,r77302,r77300,r77301,r77500,r77501&width=100%}}}
 		</div>
 		<div class="advancedSearchField">
 			Date range: <i>(e.g. 1970-1979)</i><br/>
-			{{{ca_objects.yearOfPrinting_sort%width=210px&height=40px&useDatePicker=0}}}
+			{{{ca_objects.yearOfPrinting_sort%width=100%&height=40px&useDatePicker=0}}}
 		</div>
 	</div>	
 	
 	<br style="clear: both;"/>
 	
-	<div style="float: left; margin-left: 20px; margin-right: 20px;">{{{reset%label=Reset}}}</div>
+	<div style="float: left ; margin-left: 20px; margin-right: 20px;">{{{reset%label=Reset}}}</div>
 	<div style="float: left;">{{{submit%label=Search}}}</div>
 {{{/form}}}
 
