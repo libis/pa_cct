de0a4e02925afbde1f9175de0dc89afdf9a08061
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-02-13 10:09:59 +0100

    basic search form fields width

--- themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
+++ themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
@@ -11,48 +11,48 @@
 
             <div style="float: right; margin-left: 20px; margin-right: 20px;">{{{reset%label=Reset}}}</div>
             <div style="float: right;">{{{submit%label=Search}}}</div>
-			<br style="clear: both;"/>
-			
+            <br style="clear: both;"/>
+
 			<div class='advancedContainer'>
 				<div class="advancedSearchField">
 					Free Text:<br/>
-					{{{_fulltext%width=210px&height=50px&placeholder='Type word or phrase and click Search'}}}
+					{{{_fulltext%width=100%&height=50px&placeholder='Type word or phrase and click Search'}}}
 				</div>
 				<div class="advancedSearchField">
 					Title:<br/>
-					{{{ca_objects.preferred_labels.name%width=210px}}}
+					{{{ca_objects.preferred_labels.name%width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Identifier:<br/>
-					{{{ca_objects.idno%width=210px}}}
+					{{{ca_objects.idno%width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Authors:<br/>
-					{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut,clb,edt,edc,imp,trl,oth,ppf,com,ctb&width=210px}}}
+					{{{ca_entities.preferred_labels%restrictToRelationshipTypes=aut,clb,edt,edc,imp,trl,oth,ppf,com,ctb&width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Category:<br/>
-					{{{ca_list_items.preferred_labels.name_plural%restrictToTypes=category_list&width=210px}}}
+					{{{ca_list_items.preferred_labels.name_plural%restrictToTypes=category_list&width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Source:<br/>
-					{{{ca_objects.marc900a%width=210px}}}
+					{{{ca_objects.marc900a%width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Type:<br/>
-					{{{ca_objects.type_id%width=210px}}}
+					{{{ca_objects.type_id%width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Document Type:<br/>
-					{{{ca_objects.marc655a%width=210px}}}
+					{{{ca_objects.marc655a%width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Translations:<br/>
-					{{{ca_objects_x_objects.link_translation_lkrr%excludeRelationshipTypes=r77301,r77302,r77300,r77301,r77500,r77501&width=210px}}}
+					{{{ca_objects_x_objects.link_translation_lkrr%excludeRelationshipTypes=r77301,r77302,r77300,r77301,r77500,r77501&width=100%}}}
 				</div>
 				<div class="advancedSearchField">
 					Date range: <i>(e.g. 1970-1979)</i><br/>
-					{{{ca_objects.yearOfPrinting_sort%width=210px&height=40px&useDatePicker=0}}}
+					{{{ca_objects.yearOfPrinting_sort%width=100%&height=40px&useDatePicker=0}}}
 				</div>
 			</div>
 
