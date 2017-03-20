ff9b30b78d2b43a8cae4b799f23ff77a0a7a2e7c
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-03-20 14:09:41 +0100

    field width adjustment

--- themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
+++ themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
@@ -51,7 +51,7 @@
             </div>
             <div class="col-sm-4 advancedSearchField">
                 Year From -To (e.g. 1970-1979)<br/>
-                {{{ca_objects.yearOfPrinting_sort%width=100%&height=40px&useDatePicker=0}}}
+                {{{ca_objects.yearOfPrinting_sort%width=220px&height=40px&useDatePicker=0}}}
             </div>
 				</div>
 			</div>
