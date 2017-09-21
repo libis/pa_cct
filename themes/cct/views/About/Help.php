2d7b848ef1946f6a4d0f4741e7eb8425d326d004
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-09-21 15:48:19 +0200

    help page text

--- themes/cct/views/About/Help.php
+++ themes/cct/views/About/Help.php
@@ -5,8 +5,22 @@
 
 <div class="row" style="text-align: justify; text-justify: inter-word;">
 	<div class="col-sm-8">
-		<p>Search help.</p>
+		<p>
+            <ul>
+                <li>No distinction is made between lowercase and UPPERCASE.</li>
+                <li>Search on multiple fields is performed based on AND operator.</li>
+                <li>All searches are exact term searches. <br>For example:
+                    <ul>
+                        <li><u>Christian communities</u> search on Title field will return all records that have complete and exact (excluding the case) term Christian communities in one of the titles.</li>
+                    </ul>
+                </li>
+                <li>Asterisk (*) can be appended (only at the end) to a search term to find records containing the exact term or as a substring in the value of the searched field(s). <br>For example:
+                    <ul>
+                        <li><u>gun*</u> will retrieve gun, guns, gunners, gunnery, gunning, etc.</li>
+                    </ul>
+                </li>
+                <li>For a search term less than three characters you need to suffix asterisk (*) to the search term.</li>
+            </ul>
+        </p>
 	</div>
-
-
 </div>
