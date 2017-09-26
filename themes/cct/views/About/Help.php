e3a5633686dd7da1f1103324676b3b528acaf207
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-09-26 12:20:18 +0200

    search tips text

--- themes/cct/views/About/Help.php
+++ themes/cct/views/About/Help.php
@@ -1,25 +1,46 @@
 <?php
-	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Help");
+	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Search Tips");
 ?>
-<H1><?php print _t("CCT-Database Search Tips"); ?></H1>
-
+<H1><?php print _t("Search Tips"); ?></H1>
 <div class="row" style="text-align: justify; text-justify: inter-word;">
 	<div class="col-sm-8">
 		<p>
-            <ul>
-                <li>No distinction is made between lowercase and UPPERCASE.</li>
-                <li>Search on multiple fields is performed based on AND operator.</li>
-                <li>All searches are exact term searches. <br>For example:
-                    <ul>
-                        <li><u>Christian communities</u> search on Title field will return all records that have complete and exact (excluding the case) term Christian communities in one of the titles.</li>
-                    </ul>
+            <ul  style="line-height:2;">
+                <li>
+                    Titles can be found in European languages, pinyin or characters:<br>
+                    For example: tian zhu shi yi or 天主實義
                 </li>
-                <li>Asterisk (*) can be appended (only at the end) to a search term to find records containing the exact term or as a substring in the value of the searched field(s). <br>For example:
-                    <ul>
-                        <li><u>gun*</u> will retrieve gun, guns, gunners, gunnery, gunning, etc.</li>
-                    </ul>
+                <li>
+                    For pinyin, leave spaces between syllables; place names in pinyin are without space or hyphen:<br>
+                    For example: tian zhu jiao; Shanghai, Zhongguo.
                 </li>
-                <li>For a search term less than three characters you need to suffix asterisk (*) to the search term.</li>
+                <li>
+                    Authors can be found according to Western name, pinyin or characters; there is no hyphen between two syllables in pinyin:<br>
+                    For example: Erik Zürcher or Xu Lihe or 許理和; Albert Chan or Chen Lunxu or 陳綸緒.
+                </li>
+                <li>
+                    The 'Author' search does not distinguish between types of author (editor, translator, collaborator, imprimatur, etc.).
+                </li>
+                <li>
+                    You can limit your search to source type, category or year.
+                </li>
+                <li>
+                    No distinction is made between lowercase and UPPERCASE.
+                </li>
+                <li>
+                    A search across multiple fields is performed based on AND operator.
+                </li>
+                <li>
+                    All searches are exact term searches.<br>
+                    For example: The search for <u>Christian communities</u> in the Title field will retrieve all records that have completely and exactly (excluding the case) the term <u>Christian communities</u> in one of the titles.
+                </li>
+                <li>
+                    Asterisk (*) can be appended (only at the end) to a search term in order to find records containing the exact term or as a substring in the value of the searched field(s):<br>
+                    For example: <u>Christian*</u> will retrieve Christian, Christianity, christiana, christianisme, etc.
+                </li>
+                <li>
+                    For a search term containing less than three characters one needs to suffix asterisk (*) to the search term.
+                </li>
             </ul>
         </p>
 	</div>
