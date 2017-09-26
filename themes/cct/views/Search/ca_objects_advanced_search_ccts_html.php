5b79b7db859b0c9ff11532b30eb854162a2553f9
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2017-09-26 11:49:52 +0200

    search tip link on basic and advanced search form

--- themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
+++ themes/cct/views/Search/ca_objects_advanced_search_ccts_html.php
@@ -56,14 +56,7 @@
 
 		</div>
 		<div class="col-sm-4" style='border-left:1px solid #ddd;'>
-			<h1>Search Tips</h1>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-
-            <p><a href="/pa_cct/index.php/About/Help" style="border-bottom-width: 1px; border-bottom-style: dotted; border-bottom-color: rgb(12, 120, 139); color: rgb(114, 170, 190); text-decoration: none;">More search tips</a></p>
+            <p><a href="/pa_cct/index.php/About/Help" target="_blank" style="border-bottom-width: 1px; border-bottom-color: rgb(12, 120, 139); color: rgb(114, 170, 190); text-decoration: none;">Search tips</a></p>
 		</div><!-- end col -->
 	</div><!-- end row -->
 </div><!-- end container -->
